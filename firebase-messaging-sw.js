6675e7e7491d3b0c60c89f29261bcb30db4d4a2d
Android team <68380446+android-team-stack@users.noreply.github.com>
2021-02-24 14:25:33 +0530

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/8.2.5/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.5/firebase-messaging.js");
+firebase.initializeApp({
+    apiKey: "AIzaSyA3XXiF61LANQUoGtlMVU6DeGX40IKTLCg",
+    authDomain: "llrmsg-6bd04.firebaseapp.com",
+    databaseURL: "https://llrmsg-6bd04.firebaseio.com",
+    projectId: "llrmsg-6bd04",
+    storageBucket: "llrmsg-6bd04.appspot.com",
+    messagingSenderId: "721567125675",
+    appId: "1:721567125675:web:6b4de5331f092d064627ee",
+    measurementId: "G-3K73KS66M3"
+});
+const messaging = firebase.messaging();
